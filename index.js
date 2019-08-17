ef19c3bef96d7d0efa46f66b2dc72e13458eadbf
Vitor Cortez <vitor.a.cortez@gmail.com>
2019-08-17 11:25:22 -0300

    Add message events handler

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js')
 const fs = require('fs')
+const Discord = require('discord.js')
 
 const client = new Discord.Client()
 
-let settings
 const { BOT_TOKEN: token, GUILD_ID: guildId } = process.env
+let settings
 
 try {
   const raw = fs.readFileSync('.settings')
@@ -15,57 +15,120 @@ try {
 
 client.config = { token, guildId, ...settings }
 
+client.on('ready', async () => {
+  console.log('🦉 ready to go')
+})
+
+client.on('messageDelete', async (msg) => {
+  if (msg.type !== 'DEFAULT' || msg.author.bot) {
+    return
+  }
+
+  if (client.config.whitelist) {
+    if (!client.config.whitelist.some((v) => v === msg.channel.id)) {
+      return
+    }
+  }
+
+  const embed = new Discord.RichEmbed()
+    .setDescription(
+      `**message from ${msg.author} deleted in ${msg.channel}**
+      > ${msg.cleanContent}`
+    )
+    .setAuthor(`${msg.author.tag} • ${msg.author.id}`, msg.author.displayAvatarURL)
+    .setFooter(`• ID: ${msg.id}`)
+    .setColor(16711680)
+    .setTimestamp()
+
+  client.config.logs.messages.send(embed)
+})
+
+client.on('messageUpdate', async (old, msg) => {
+  if (msg.type !== 'DEFAULT' || msg.author.bot) {
+    return
+  }
+
+  if (client.config.whitelist) {
+    if (!client.config.whitelist.some((v) => v === msg.channel.id)) {
+      return
+    }
+  }
+
+  const embed = new Discord.RichEmbed()
+    .setDescription(
+      `**message edited in ${msg.channel}**
+      [message link](${msg.url})`
+    )
+    .setAuthor(`${msg.author.tag} • ${msg.author.id}`, msg.author.displayAvatarURL)
+    .addField('before', `> ${old.cleanContent}`)
+    .addField('after', `> ${msg.cleanContent}`)
+    .setFooter(`• ID: ${msg.id}`)
+    .setColor(255)
+    .setTimestamp()
+
+  client.config.logs.messages.send(embed)
+})
+
 client.login(client.config.token)
   .then(() => {
     const guild = client.guilds.get(client.config.guildId)
 
-    if (guild.available && client.config.menus) {
-      for (const menu of client.config.menus) {
-        const channel = guild.channels.get(menu.channel)
-        const roles = Object.keys(menu.roles)
-        const emojis = Object.values(menu.roles)
+    if (guild.available) {
+      if (client.config.logs) {
+        for (const key in client.config.logs) {
+          const id = client.config.logs[key]
+          client.config.logs[key] = guild.channels.get(id)
+        }
+      }
 
-        channel.fetchMessage(menu.message)
-          .then(async (message) => {
-            try {
-              for (const emoji of emojis) {
-                await message.react(emoji)
-              }
-            } catch (err) {
-              console.error('a reaction failed')
-            }
+      if (client.config.menus) {
+        for (const menu of client.config.menus) {
+          const channel = guild.channels.get(menu.channel)
+          const roles = Object.keys(menu.roles)
+          const emojis = Object.values(menu.roles)
 
-            const filter = (r, user) => {
-              if (emojis.includes(r.emoji.name) && !user.bot) {
-                r.user = user
-                return true
-              }
-              return false
-            }
-
-            const collector = message.createReactionCollector(filter)
-
-            collector.on('collect', (r) => {
-              const emoji = r.emoji.name
-              const user = r.user.id
-              console.log(`collect: user '${user}' reacted with emoji ${emoji}`)
-
-              if (guild.available) {
-                const member = guild.members.get(user)
-
-                if (!member) return
-
-                if (member.roles.some((r) => roles.includes(r.name))) {
-                  return
+          channel.fetchMessage(menu.message)
+            .then(async (message) => {
+              try {
+                for (const emoji of emojis) {
+                  await message.react(emoji)
                 }
-
-                const role = guild.roles.find((r) => r.name === roles[emojis.indexOf(emoji)])
-
-                member.addRole(role).catch(console.error)
+              } catch (err) {
+                console.error('a reaction failed')
               }
+
+              const filter = (r, user) => {
+                if (emojis.includes(r.emoji.name) && !user.bot) {
+                  r.user = user
+                  return true
+                }
+                return false
+              }
+
+              const collector = message.createReactionCollector(filter)
+
+              collector.on('collect', (r) => {
+                const emoji = r.emoji.name
+                const user = r.user.id
+                console.log(`collect: user '${user}' reacted with emoji ${emoji}`)
+
+                if (guild.available) {
+                  const member = guild.members.get(user)
+
+                  if (!member) return
+
+                  if (member.roles.some((r) => roles.includes(r.name))) {
+                    return
+                  }
+
+                  const role = guild.roles.find((r) => r.name === roles[emojis.indexOf(emoji)])
+
+                  member.addRole(role).catch(console.error)
+                }
+              })
+              collector.on('end', (c) => console.log(`end: collected ${c.size} items`))
             })
-            collector.on('end', (c) => console.log(`end: collected ${c.size} items`))
-          })
+        }
       }
     }
   })
