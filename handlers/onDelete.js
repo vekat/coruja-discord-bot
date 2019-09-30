7bc1d68dce64632e455ee9faa69662fb52be3722
Vitor Cortez <vitor.a.cortez@gmail.com>
2019-09-30 17:34:09 -0300

    Update message logging

--- handlers/onDelete.js
+++ handlers/onDelete.js
@@ -1,4 +1,4 @@
-const { RichEmbed } = require('discord.js')
+const { RichEmbed, Attachment } = require('discord.js')
 
 const { chain } = require('../utils/chain')
 const { ignoreArtificial, ensureWhitelist } = require('../utils/helpers')
@@ -13,17 +13,24 @@ exports.run = async (client, message) => {
 }
 
 async function logMessage({ client, message: msg }) {
-  const embed = new RichEmbed()
-    .setDescription(
-      `**message from ${msg.author} deleted in ${msg.channel}**\n>>> ${msg.cleanContent}`
-    )
-    .setAuthor(`${msg.author.tag} • ${msg.author.id}`, msg.author.displayAvatarURL)
-    .setFooter(`• ID: ${msg.id}`)
-    .setColor(16711680)
-    .setTimestamp()
-
   if (client.config.logs['messages']) {
-    client.config.logs.messages.send(embed)
+    const embed = new RichEmbed()
+      .setDescription(
+        `**message from ${msg.author} deleted in ${msg.channel}**\n>>> ${msg.cleanContent || "NO TEXT"}`
+      )
+      .setAuthor(`${msg.author.tag} • ${msg.author.id}`, msg.author.displayAvatarURL)
+      .setFooter(`• ID: ${msg.id}`)
+      .setColor(16711680)
+      .setTimestamp()
+
+    const opts = { embed }
+    if (msg.attachments.size > 0) {
+      msg.attachments.tap((v, _) => {
+        embed.addField(`attachment: *${v.filename}*`, `${v.proxyURL}`)
+      })
+    }
+
+    client.config.logs.messages.send(opts)
   }
 }
 
