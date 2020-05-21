6265104c03fdbc7cb010f042193a1c580f2a5dc3
Vitor Cortez <vitor.a.cortez@gmail.com>
2020-05-20 21:58:04 -0300

    Add voice state handlers

---
+++ handlers/onJoinVC.js
@@ -0,0 +1,48 @@
+const { chain } = require('../utils/chain')
+const { getBaseCtx } = require('../utils/contexts')
+
+exports.meta = {
+  event: 'voiceStateUpdate'
+}
+
+exports.run = async (client, oldState, newState) => {
+  const ctx = getBaseCtx(client, this.meta, { oldState, newState })
+
+  return chain(checkJoin, delay, isConnected, assignRole)(ctx)
+    .then(ctx.onSuccess)
+    .catch(ctx.onError)
+}
+
+async function checkJoin({ oldState: o, filter }, next) {
+  return (o.channel) ? filter('not joining') : next()
+}
+
+async function delay(_, next) {
+  await timeout(5000)
+  return next()
+}
+
+async function isConnected({ newState: n, filter }, next) {
+  if (n.channel) {
+    return next()
+  }
+  filter('not connected')
+}
+
+async function assignRole({ newState: n, settings, guild }) {
+  if (!settings.voicerole) return
+
+  const roleID = settings.voicerole
+  const role = guild.roles.cache.get(roleID)
+  if (!role) return
+
+  const member = n.member
+  if (!member) return
+  if (member.roles.cache.has(roleID)) return
+
+  member.roles.add(role, 'joined voice chat')
+}
+
+function timeout(ms) {
+  return new Promise((res) => setTimeout(res, ms))
+}
---
+++ handlers/onQuitVC.js
@@ -0,0 +1,48 @@
+const { chain } = require('../utils/chain')
+const { getBaseCtx } = require('../utils/contexts')
+
+exports.meta = {
+  event: 'voiceStateUpdate'
+}
+
+exports.run = async (client, oldState, newState) => {
+  const ctx = getBaseCtx(client, this.meta, { oldState, newState })
+
+  return chain(checkQuit, delay, isDisconnected, removeRole)(ctx)
+    .then(ctx.onSuccess)
+    .catch(ctx.onError)
+}
+
+async function checkQuit({ oldState: o, newState: n, filter }, next) {
+  return (!o.channel || n.channel) ? filter('not quitting') : next()
+}
+
+async function delay(_, next) {
+  await timeout(5000)
+  return next()
+}
+
+async function isDisconnected({ newState: n, filter }, next) {
+  if (!n.channel) {
+    return next()
+  }
+  filter('not disconnected')
+}
+
+async function removeRole({ newState: n, settings, guild }) {
+  if (!settings.voicerole) return
+
+  const roleID = settings.voicerole
+  const role = guild.roles.cache.get(roleID)
+  if (!role) return
+
+  const member = n.member
+  if (!member) return
+  if (!member.roles.cache.has(roleID)) return
+
+  member.roles.remove(role, 'left voice chat')
+}
+
+function timeout(ms) {
+  return new Promise((res) => setTimeout(res, ms))
+}
